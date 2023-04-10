0f4e2bddffab3faa735942a6584382cb940c5f8e
polidor01 <polinadoratis@gmail.com>
2023-04-10 17:48:16 +0200

    1.4

--- todo.js
+++ todo.js
@@ -1,10 +1,4 @@
-const todoContainer = document.querySelector('.todo-container');
-const todoInput = document.querySelector('.inputField');
-const todoItemsList = document.querySelector('.todo-items');
 
-
-//storing all the items by creating an array//
-let todos = [];
 
 
 
